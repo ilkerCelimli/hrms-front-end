951cdabf34cb602d4b52de4f3d920f7beb47871b
ilker <meloonia52@gmail.com>
2021-07-20 15:44:48 +0300

    Güncelleme ula güncelleme

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import AdminSidebar from './layouts/components/sidebars/AdminSidebar';
 import Navbar from './layouts/components/navbars/Navbar'
 import LoginPage from './pages/LoginPage';
-import Footer from './layouts/Footer';
 import { Container } from 'reactstrap';
 function App() {
   return (
@@ -11,7 +10,7 @@ function App() {
 
     <Container className="themed-container"> <Navbar/> </Container>
     <Container className = 'themed-container' fluid = 'sm'><AdminSidebar/> </Container>
-    </div>
+    </div> 
   );
 }
 
