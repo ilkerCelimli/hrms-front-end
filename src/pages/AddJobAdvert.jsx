eeb34e54cd2125ed494aeb85c3ee0adbe37a47d5
ilker <meloonia52@gmail.com>
2021-07-23 21:50:44 +0300

    update

--- src/pages/AddJobAdvert.jsx
+++ src/pages/AddJobAdvert.jsx
@@ -28,17 +28,18 @@ export default function AddJobAdvert() {
     const [jobAdverts,setJobAdverts] = useState([]);
     const {handleChange,values,handleSubmit} = useFormik({
         initialValues:{
-           
-            relaseDate : '',
-            activeDate : '',
             businessSector : '',
-            city : ''.charAt(0).toLocaleUpperCase(),
-            openPosition : '',
-            minSalary: '',
-            maxSalary : '',
-            desciription : '',
-            active: 'false',
-            workType : '',
+           activeDate : '',
+           openPosition :'',
+           minSalary : '',
+           maxSalary : '',
+           active : 'false',
+           desciription : '',
+           cityName : '',
+           workType : ''
+
+           
+
             
         } ,validationSchema ,onSubmit : values =>(JSON.stringify(values,7,null))
     })
@@ -47,8 +48,8 @@ export default function AddJobAdvert() {
     useEffect(()=> {
         let employerService = new EmployerService();
 
-        employerService.addJobAdvert(values).then(result => setJobAdverts(result.data))
-       
+        employerService.addJobAdvert(values).then(result => setJobAdverts(result.data.data))
+        
         
     })
     return (
@@ -59,7 +60,7 @@ export default function AddJobAdvert() {
             
                 <FormGroup>
                     <Label>İş Sektörü</Label>
-                    <Input type ='select' name ='businessSector' value = {values.businessSector}>
+                    <Input type ='select' name ='businessSector' onChange = {handleChange}>
                         <option>Yazılım Mühendisliği</option>
                         <option>Back End developer</option>
                         <option>Front end Developer</option>
@@ -68,28 +69,28 @@ export default function AddJobAdvert() {
                 
                 <FormGroup>
                     <Label>İlan tarihi</Label>
-                    <DatePicker selected = {relaseDate} onChange = {(date) => setRelaseDate(date)}/>
+                    <DatePicker selected = {relaseDate} onChange = {(date) => setRelaseDate(date)} onChange = {handleChange}/>
                 </FormGroup>
 
                 <FormGroup>
                     <Label>Aktiflik tarihi</Label>
-                    <DatePicker selected = {activeDate} onChange = {(date) => setActiveDate(date)} />
+                    <DatePicker selected = {activeDate} onChange = {(date) => setActiveDate(date)} onChange = {handleChange} />
                 </FormGroup>
 
                 <FormGroup>
                     <Label>Açık Pozisyon</Label>
-                    <Input type = 'text' name = 'openPosition'> </Input>
+                    <Input type = 'text' name = 'openPosition' onChange = {handleChange}> </Input>
                 </FormGroup>
 
                 <FormGroup>
                     <Label>Minimum - Maksimum Fiyat</Label>
-                    <Input type = 'text' name = 'minSalary' placeholder = 'Minimum fiyat'/>
-                    <Input type = 'text' name ='maxSalary' placeholder = 'Maksimum fiyat'/>
+                    <Input type = 'text' name = 'minSalary' placeholder = 'Minimum fiyat' onChange = {handleChange}/>
+                    <Input type = 'text' name ='maxSalary' placeholder = 'Maksimum fiyat' onChange = {handleChange}/>
                 </FormGroup>
 
                 <FormGroup>
                     <Label>Çalışma Şekli</Label>
-                    <Input type ='select' name ='workType'>
+                    <Input type ='select' name ='workType' onChange = {handleChange}>
                         <option>Remote</option>
                         <option>Yarı Remote</option>
                         <option>Tam zamanlı</option>
@@ -99,12 +100,12 @@ export default function AddJobAdvert() {
 
                 <FormGroup>
                     <Label>Şehir</Label>
-                    <Input type = 'text'name = 'city'/>
+                    <Input type = 'text'name = 'city' onChange = {handleChange}/>
                 </FormGroup>
 
                 <FormGroup>
                     <Label>İş açıklaması</Label>
-                    <Input type = 'text' name = 'desciription'/>
+                    <Input type = 'text' name = 'desciription' onChange = {handleChange} />
                 </FormGroup>
                 <Button color ='primary' onSubmit = {handleSubmit}>Kaydet</Button>
             </Form> 
