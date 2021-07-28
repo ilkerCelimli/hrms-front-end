bc6f62c43d40c4a2f67cc58e16b09c0171812c9c
ilker <meloonia52@gmail.com>
2021-07-28 09:40:05 +0300

    Formlar Oauth işlemlerinden sonra Back Ende bağlanıcaktır.

--- src/layouts/components/forms/JobSeekerForm/AddGithubForm.jsx
+++ src/layouts/components/forms/JobSeekerForm/AddGithubForm.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import { useFormik } from 'formik'
+import React, { useState } from 'react'
+import { Form, Label ,Input, FormGroup,Button } from 'reactstrap'
+import * as Yup from 'yup'
+
+
+const validationSchema = Yup.object().shape( {
+    jobSeekerGithubAdress : Yup.string().required("Github adresi zorunlu")
+})
+
 
 export default function AddGithubForm() {
+
+
+    const [values,handleChange,handleSubmit] = useFormik({
+        initialValues : {
+            jobSeekerGithubAdress : ''
+        } , validationSchema : validationSchema ,onSubmit: (values) => {console.log(values)}
+    })
+
+    const [id,setId] = useState(0)
+
     return (
         <div>
             
-            
+          <Form onSubmit = {handleSubmit}>
+              <FormGroup>
+              <Label>github Adresi</Label>
+              <Input type = 'text' name = 'jobSeekerGithubAdress' onChange = {handleChange}/>
+              </FormGroup>
+              
+              <Button type ='submit' onSubmit = {handleSubmit} onClick = {() => {
+                  //Axios kodları.
+              }}> Ekle </Button>
+          </Form>
 
         </div>
     )
--- src/layouts/components/forms/JobSeekerForm/addLinkedLnForm.jsx
+++ src/layouts/components/forms/JobSeekerForm/addLinkedLnForm.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useFormik } from 'formik'
+import React, { useState } from 'react'
+import { Form, Label ,Input, FormGroup,Button } from 'reactstrap'
+import * as Yup from 'yup'
 
-export default function addLinkedLnForm() {
+
+const validationSchema = Yup.object().shape( {
+    jobSeekerLinkedlnAdress : Yup.string().required("linkedln adresi zorunlu")
+})
+
+
+export default function AddGithubForm() {
+
+
+    const [values,handleChange,handleSubmit] = useFormik({
+        initialValues : {
+            jobSeekerLinkedlnAdress : ''
+        } , validationSchema : validationSchema ,onSubmit: (values) => {console.log(values)}
+    })
+
+    const [id,setId] = useState(0)
+
     return (
         <div>
             
+          <Form onSubmit = {handleSubmit}>
+              <FormGroup>
+              <Label>github Adresi</Label>
+              <Input type = 'text' name = 'jobSeekerLinkedlnAdress' onChange = {handleChange}/>
+              </FormGroup>
+              
+              <Button type ='submit' onSubmit = {handleSubmit} onClick = {() => {
+                  //Axios kodları.
+              }}> Ekle </Button>
+          </Form>
+
         </div>
     )
 }
